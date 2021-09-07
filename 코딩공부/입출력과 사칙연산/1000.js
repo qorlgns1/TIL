25f8366b73e2cb77c78b407eb6b8c982b7d27ddb
qorlgns1 <rlgns0610@gmail.com>
2021-09-07 11:48:21 +0900

    코딩공부

---
+++ 코딩공부/입출력과 사칙연산/1000.js
@@ -0,0 +1,5 @@
+let fs = require('fs');
+let [a, b] = fs.readFileSync('../예제.txt').toString().split(' ').map(v => parseInt(v));
+//let [a, b] = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => parseInt(value));
+
+console.log(a+b);
