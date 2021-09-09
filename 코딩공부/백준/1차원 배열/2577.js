33088fcd52a93ce713c827b5f8443d38c1bdf3f8
qorlgns1 <rlgns0610@gmail.com>
2021-09-09 21:19:17 +0900

    코딩공부

--- 코딩공부/백준/1차원 배열/2577.js
+++ 코딩공부/백준/1차원 배열/2577.js
@@ -13,3 +13,19 @@ for (let i = 0; i < m.length; i++) {
 }
 
 console.log(array.join("\n"));
+
+/* 다른 사람 코드
+const [a, b, c] = require('fs')
+    .readFileSync('dev/stdin')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(v => +v);
+
+let result = Array(10).fill(0);
+let num = `${a * b * c}`
+    .split('')
+    .forEach(n => {result[n] += 1;});
+
+console.log(result.join('\n'));
+*/
