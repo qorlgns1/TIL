c61f752b9001e26a4228bb762d754765a25b9db7
qorlgns1 <rlgns0610@gmail.com>
2021-10-07 10:47:41 +0900

    코딩공부

--- 코딩공부/백준/기본 수학 2/11653.js
+++ 코딩공부/백준/기본 수학 2/11653.js
@@ -1,4 +1,29 @@
 let fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../예제.txt';
-let number = require('fs').readFileSync(filePath).toString().trim();
-number = Number(number);
+let input = require('fs').readFileSync(filePath).toString().trim();
+input = Number(input);
+
+function solution(number) {
+  if (number === 1) {
+    return;
+  }
+
+  let LAST_EVEN = 2;
+  while (number % LAST_EVEN === 0) {
+    console.log(LAST_EVEN);
+    number /= LAST_EVEN;
+  }
+
+  for (let odd = 3; odd <= number; odd = odd + 2) {
+    while (number % odd === 0) {
+      console.log(odd);
+      number /= odd;
+    }
+
+    if (number === 1) {
+      break;
+    }
+  }
+}
+
+solution(input);
