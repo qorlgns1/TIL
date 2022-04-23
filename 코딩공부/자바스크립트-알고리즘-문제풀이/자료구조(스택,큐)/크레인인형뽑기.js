023ab5546139b9ac743b533900530d4de3e306c1
qorlgns1 <rlgns0610@gmail.com>
2022-04-23 23:31:26 +0900

    스택 알고리즘 공부

--- 코딩공부/자바스크립트-알고리즘-문제풀이/자료구조(스택,큐)/크레인인형뽑기.js
+++ 코딩공부/자바스크립트-알고리즘-문제풀이/자료구조(스택,큐)/크레인인형뽑기.js
@@ -1,27 +1,51 @@
-// 카카오 문제
+// 카카오 크레인 인형뽑기 문제
+// https://programmers.co.kr/learn/courses/30/lessons/64061
 // stack 이용
 
-function solution(board, moves) {
+//풀이 1
+// function solution(board, moves) {
+//   let answer = 0;
+//   let stack = [];
+//   moves.forEach((pos) => {
+//     for (let i = 0; i < board.length; i++) {
+//       if (board[i][pos - 1] !== 0) {
+//         let tmp = board[i][pos - 1];
+//         board[i][pos - 1] = 0;
+//         if (tmp === stack[stack.length - 1]) {
+//           stack.pop();
+//           answer += 2;
+//         } else {
+//           stack.push(tmp);
+//         }
+//         break;
+//       }
+//     }
+//   });
+
+//   return answer;
+// }
+
+//풀이 2
+const solution = (board, moves) => {
   let answer = 0;
   let stack = [];
-  moves.forEach((pos) => {
-    for (let i = 0; i < board.length; i++) {
-      if (board[i][pos - 1] !== 0) {
-        let tmp = board[i][pos - 1];
-        board[i][pos - 1] = 0;
-        if (tmp === stack[stack.length - 1]) {
+
+  for (let i = 0; i < moves.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      if (board[j][moves[i] - 1] > 0) {
+        if (stack[stack.length - 1] === stack[stack.length - 2]) {
           stack.pop();
-          answer += 2;
-        } else {
-          stack.push(tmp);
+          answer++;
         }
+
+        board[j][moves[i] - 1] = 0;
         break;
       }
     }
-  });
+  }
 
-  return answer;
-}
+  return answer * 2;
+};
 
 let a = [
   [0, 0, 0, 0, 0],
