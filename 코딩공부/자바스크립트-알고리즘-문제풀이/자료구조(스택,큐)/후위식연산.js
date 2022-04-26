a723258b54bab1e0550b47cc7abaf939728a9e8c
qorlgns1 <rlgns0610@gmail.com>
2022-04-26 21:15:11 +0900

    스택 알고리즘 공부

--- 코딩공부/자바스크립트-알고리즘-문제풀이/자료구조(스택,큐)/후위식연산.js
+++ 코딩공부/자바스크립트-알고리즘-문제풀이/자료구조(스택,큐)/후위식연산.js
@@ -1,26 +1,79 @@
-function solution(s) {
-  let answer;
+// ?��?��?�� ?��?��(postfix)
+// ?��?��?��?��?��?�� 주어�?�? ?��?��?�� 결과�? 출력?��?�� ?��로그?��?�� ?��?��?��?��?��.
+// 만약 3*(5+2)-9 ?�� ?��?��?��?��?��?���? ?��?��?���? 352+*9- �? ?��?��?���? �? 결과?�� 12?��?��?��.
+
+// ?�� ?��?��?���?
+// �? 줄에 ?��?��?��?��?��?�� 주어집니?��. ?��?��?��?�� 길이?�� 50?�� ?���? ?��?��?��?��.
+// ?��??? 1~9?�� ?��?��??? +, -, *, / ?��?��?��로만 ?��루어진다.
+
+// ?�� 출력?���?
+// ?��?��?�� 결과�? 출력?��?��?��.
+
+// ?�� ?��?��?��?�� 1
+// 352+*9-
+
+// ?�� 출력?��?�� 1
+// 12
+
+// function solution(s) {
+//   let answer;
+//   let stack = [];
+//   for (let x of s) {
+//     if (!isNaN(x)) {
+//       stack.push(Number(x));
+//     } else {
+//       let rt = stack.pop();
+//       let lt = stack.pop();
+//       if (x === "+") {
+//         stack.push(lt + rt);
+//       } else if (x === "-") {
+//         stack.push(lt - rt);
+//       } else if (x === "*") {
+//         stack.push(lt * rt);
+//       } else if (x === "/") {
+//         stack.push(lt / rt);
+//       }
+//     }
+//   }
+//   answer = stack[0];
+//   return answer;
+// }
+
+const solution = (str) => {
   let stack = [];
-  for (let x of s) {
-    if (!isNaN(x)) {
-      stack.push(Number(x));
+
+  for (const ch of str) {
+    if (!isNaN(ch)) {
+      stack.push(parseInt(ch));
     } else {
       let rt = stack.pop();
       let lt = stack.pop();
-      if (x === "+") {
-        stack.push(lt + rt);
-      } else if (x === "-") {
-        stack.push(lt - rt);
-      } else if (x === "*") {
-        stack.push(lt * rt);
-      } else if (x === "/") {
-        stack.push(lt / rt);
+
+      switch (ch) {
+        case "+":
+          stack.push(lt + rt);
+          break;
+
+        case "-":
+          stack.push(lt - rt);
+          break;
+
+        case "*":
+          stack.push(lt * rt);
+          break;
+
+        case "/":
+          stack.push(lt / rt);
+          break;
+
+        default:
+          return console.log("error");
       }
     }
   }
-  answer = stack[0];
-  return answer;
-}
+
+  return stack[0];
+};
 
 let str = "352+*9-";
 console.log(solution(str));
