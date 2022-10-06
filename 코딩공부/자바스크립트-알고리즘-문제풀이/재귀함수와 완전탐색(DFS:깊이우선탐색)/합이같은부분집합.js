6d1203887ffdfa6c43c1ac67a1c95e973c28cf6e
qorlgns1 <rlgns0610@gmail.com>
2022-10-06 09:17:28 +0900

    알고리즘 문제풀이

--- 코딩공부/자바스크립트-알고리즘-문제풀이/재귀함수와 완전탐색(DFS:깊이우선탐색)/합이같은부분집합.js
+++ 코딩공부/자바스크립트-알고리즘-문제풀이/재귀함수와 완전탐색(DFS:깊이우선탐색)/합이같은부분집합.js
@@ -1,29 +1,69 @@
 // N개의 자연수로 구성된 집합을 두개의 부분집합으로 나눌 때 합이 같은것이 있으면 YES를 출력 없으면 NO 출력
-const solution = (arr) => {
-  let result = "NO";
-  const total = arr.reduce((a, b) => a + b, 0);
-  let flag = 0;
-  const n = arr.length;
+// const solution = (arr) => {
+//   let result = "NO";
+//   const total = arr.reduce((a, b) => a + b, 0);
+//   let flag = 0;
+//   const n = arr.length;
 
-  const DFS = (L, sum) => {
+//   const DFS = (L, sum) => {
+//     if (flag) {
+//       return;
+//     }
+//     if (L === n) {
+//       if (total - sum === sum) {
+//         result = "YES";
+//         flag = 1;
+//       }
+//     } else {
+//       DFS(L + 1, sum + arr[L]);
+//       DFS(L + 1, sum);
+//     }
+//   };
+
+//   DFS(0, 0);
+
+//   return result;
+// };
+
+function solution(arr) {
+  const ch = new Array(arr.length).fill(0);
+  const answer = [];
+  const total = arr.reduce((acc, cur) => acc + cur, 0);
+
+  let flag = false;
+
+  function DFS(i) {
     if (flag) {
       return;
     }
-    if (L === n) {
+
+    if (i >= arr.length) {
+      // console.log(ch.join(' '));
+
+      let sum = 0;
+      ch.forEach((v, i) => {
+        if (ch[i] > 0) {
+          sum += arr[i];
+        }
+      });
+
       if (total - sum === sum) {
-        result = "YES";
-        flag = 1;
+        // console.log(ch);
+        flag = true;
       }
     } else {
-      DFS(L + 1, sum + arr[L]);
-      DFS(L + 1, sum);
+      ch[i] = 1;
+      DFS(i + 1);
+      ch[i] = 0;
+      DFS(i + 1);
     }
-  };
+  }
 
-  DFS(0, 0);
+  DFS(0);
+  return answer;
+}
 
-  return result;
-};
+// console.log(solution([1, 3, 5, 6, 7, 10]));
 
 let arr = [1, 3, 5, 6, 7, 10];
 console.log(solution(arr));
