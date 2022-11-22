27c16aee2d4756efe834e70523fe407f2ca1d871
qorlgns1 <rlgns0610@gmail.com>
2022-11-22 12:51:01 +0900

    알고리즘 문제풀이

--- 코딩공부/자바스크립트-알고리즘-문제풀이/동적계획법/최대부분증가수열(LIS).js
+++ 코딩공부/자바스크립트-알고리즘-문제풀이/동적계획법/최대부분증가수열(LIS).js
@@ -1,24 +1,42 @@
 // 미해결
 
+// function solution(arr) {
+//   const dy = [1];
+
+//   for (let i = 1; i < arr.length; i++) {
+//     const front = arr.slice(0, i);
+//     const max = front.reduce((a, c) => {
+//       if (arr[i] > c && a <= c) {
+//         return c;
+//       }
+//       return a;
+//     }, -1);
+
+//     const lastIndex = front.lastIndexOf(max);
+
+//     if (lastIndex < 0) {
+//       dy.push(1);
+//     } else {
+//       dy.push(dy[lastIndex] + 1);
+//     }
+//   }
+
+//   return Math.max(...dy);
+// }
+
 function solution(arr) {
-  const dy = [1];
+  const dy = new Array(arr.length).fill(0);
+  dy[0] = 1;
 
   for (let i = 1; i < arr.length; i++) {
-    const front = arr.slice(0, i);
-    const max = front.reduce((a, c) => {
-      if (arr[i] > c && a <= c) {
-        return c;
+    let maxCount = 0;
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[j] < arr[i] && dy[j] > maxCount) {
+        maxCount = dy[j];
       }
-      return a;
-    }, -1);
-
-    const lastIndex = front.lastIndexOf(max);
-
-    if (lastIndex < 0) {
-      dy.push(1);
-    } else {
-      dy.push(dy[lastIndex] + 1);
     }
+
+    dy[i] = maxCount + 1;
   }
 
   return Math.max(...dy);
@@ -26,3 +44,6 @@ function solution(arr) {
 
 // console.log(solution([5, 3, 7, 8, 6, 2, 9, 4]));
 console.log(solution([2, 7, 5, 8, 6, 4, 7, 12, 3]));
+
+// [2, 7, 5, 8, 6, 4, 7, 12, 3]
+// [1, 2, 2, 3, 3, 2, 4,  5, 2]
