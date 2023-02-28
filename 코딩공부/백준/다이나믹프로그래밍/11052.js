0e7b1db2d1a2cf6f92d2c8451ed5dc95997e2e9e
qorlgns1 <rlgns0610@gmail.com>
2023-02-28 23:20:00 +0900

    알고리즘 문제풀이

---
+++ 코딩공부/백준/다이나믹프로그래밍/11052.js
@@ -0,0 +1,18 @@
+let fs = require("fs");
+let [N, cards] = fs
+  .readFileSync("../예제.txt")
+  // .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(`\n`);
+
+let cardPacks = cards.split(" ").map(Number);
+const dy = [0, ...cardPacks];
+
+for (let i = 2; i <= cardPacks.length; i++) {
+  for (let j = 1; j < i; j++) {
+    dy[i] = Math.max(dy[i], dy[i - j] + dy[j]);
+  }
+}
+
+console.log(dy[N]);
