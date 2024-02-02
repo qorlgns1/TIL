571b657a71ca0b56318ae5014dd0a8a7f2e49a63
qorlgns1 <rlgns0610@gmail.com>
2024-02-02 21:56:13 +0900

    알고리즘 문제풀이

---
+++ 코딩공부/프로그래머스/Lv3/징검다리.js
@@ -0,0 +1,34 @@
+function solution(distance, rocks, n) {
+  let answer = 0;
+  let min = 1;
+  let max = distance;
+  rocks.sort((a, b) => a - b);
+
+  while (min <= max) {
+    const mid = Math.floor((min + max) / 2);
+    let remove = 0;
+    let prev = 0;
+
+    for (let i = 0; i < rocks.length; i++) {
+      if (rocks[i] - prev <= mid) {
+        remove++;
+      } else {
+        prev = rocks[i];
+      }
+    }
+
+    if (remove > n) {
+      max = mid - 1;
+    } else {
+      min = mid + 1;
+      answer = Math.max(answer, min);
+    }
+  }
+
+  return answer;
+}
+
+const distance = 25;
+const rocks = [2, 14, 11, 21, 17];
+const n = 2;
+console.log(solution(distance, rocks, n)); // 4
