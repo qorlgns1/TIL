d3767483303588b61eb835996fbe67f83a8cd014
qorlgns1 <rlgns0610@gmail.com>
2024-03-10 11:38:47 +0900

    알고리즘 문제풀이

---
+++ 코딩공부/백준/BFS/1697.js
@@ -0,0 +1,31 @@
+// https://www.acmicpc.net/problem/1697
+
+let fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../예제.txt';
+let [N, K] = fs.readFileSync(filePath).toString().trim().split(' ').map(Number);
+
+const MAX_LEN = 100000;
+const ch = new Array(MAX_LEN + 1).fill(0);
+const queue = [[N, 0]];
+let answer = 0;
+
+while (queue.length) {
+  const [x, cnt] = queue.shift();
+
+  // 지나갔던 길이면 넘어감
+  if (ch[x]) continue;
+
+  if (x === K) {
+    answer = cnt;
+    break;
+  } else {
+    ch[x] = 1;
+    answer++;
+
+    if (x * 2 <= MAX_LEN && !ch[x * 2]) queue.push([x * 2, cnt + 1]);
+    if (x + 1 <= MAX_LEN && !ch[x + 1]) queue.push([x + 1, cnt + 1]);
+    if (x - 1 >= 0 && !ch[x - 1]) queue.push([x - 1, cnt + 1]);
+  }
+}
+
+console.log(answer);
