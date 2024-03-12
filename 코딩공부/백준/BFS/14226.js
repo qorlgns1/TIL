c45260a533fa0053ad88281194076a87bfb9b46b
qorlgns1 <rlgns0610@gmail.com>
2024-03-12 13:09:20 +0900

    알고리즘 문제풀이

---
+++ 코딩공부/백준/BFS/14226.js
@@ -0,0 +1,37 @@
+let fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../예제.txt';
+const S = Number(fs.readFileSync(filePath).toString().trim());
+
+let answer = 0;
+const queue = [[1, 0, 0]];
+const ch = Array.from({ length: 1001 }, () => [0, 0]);
+ch[1][0] = 1;
+
+while (queue.length) {
+  const [imojiCnt, time, clipCnt] = queue.shift();
+
+  if (imojiCnt === S) {
+    answer = time;
+    break;
+  }
+
+  const nextTime = time + 1;
+
+  // 클립보드 복사
+  if (!ch[imojiCnt][imojiCnt]) {
+    ch[imojiCnt][imojiCnt] = 1;
+    queue.push([imojiCnt, nextTime, imojiCnt]);
+  }
+
+  const nextEmojiCntArr = [imojiCnt + clipCnt, imojiCnt - 1].filter((cnt) => {
+    return cnt <= 1000 && cnt >= 0 && !ch[cnt][clipCnt];
+  });
+
+  // 클립보드 복사 안함
+  nextEmojiCntArr.forEach((nextEmojiCnt) => {
+    ch[nextEmojiCnt][clipCnt] = 1;
+    queue.push([nextEmojiCnt, nextTime, clipCnt]);
+  });
+}
+
+console.log(answer);
