0f338c0b7f0c800784e6c4ac6318f9e23944241c
qorlgns1 <rlgns0610@gmail.com>
2024-03-12 11:31:06 +0900

    알고리즘 문제풀이

--- 코딩공부/백준/BFS/12851.js
+++ 코딩공부/백준/BFS/12851.js
@@ -2,7 +2,9 @@
 // 문제가 잘못된 것 같다.
 // 1 10을 넣었을 때 답이 4와 1이 나와야 한다고 생각한다.
 // 통과되는 답은 4 2인데 마지막 2의 경로를 보면 동일하기 때문에 하나의 경로라고 볼 수 있다고 생각한다.
+// 문제가 잘못됬다고 기여해보고 싶은데, 등급미달로 기여하는 시도를 못하는게 아쉽다...ㅠㅠ
 
+// 통과하는 풀이를 가져옴
 let fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../예제.txt';
 let [N, K] = fs.readFileSync(filePath).toString().trim().split(' ').map(Number);
