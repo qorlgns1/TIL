91e8029f6d7538f6aac4b5ddd7084e492ded8fb6
qorlgns1 <rlgns0610@gmail.com>
2024-03-24 17:06:01 +0900

    질문에 대한 멋진 답변 기록

--- 코딩공부/백준/그래프/18352.js
+++ 코딩공부/백준/그래프/18352.js
@@ -1,6 +1,9 @@
 // https://www.acmicpc.net/problem/18352
 // https://brick-parrot-a90.notion.site/872667e5598d49f7a2757f8004c25869
 
+// 질문에 대한 멋진 답변 기록
+// https://www.acmicpc.net/board/view/122676
+
 let fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../예제.txt';
 let [[N, _, K, X], ...arr] = fs
