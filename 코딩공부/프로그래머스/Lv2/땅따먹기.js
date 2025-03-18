82b19048c4c5a3a2a1ac10df8016cf3f6d87aba6
qorlgns1 <rlgns0610@gmail.com>
2025-03-18 15:01:12 +0900

    알고리즘 문제풀이

--- 코딩공부/프로그래머스/Lv2/땅따먹기.js
+++ 코딩공부/프로그래머스/Lv2/땅따먹기.js
@@ -1,5 +1,36 @@
-// 나중에 다시 풀어보자
+// // BFS 풀이 시간초과
+// function solution(land) {
+//   const MAX_LAND_DEPTH = land.length;
+//   let maxPointSum = 0;
 
+//   function bfs(pointSum, positionIndex, depth) {
+//     if (depth === MAX_LAND_DEPTH) {
+//       if (maxPointSum < pointSum) {
+//         maxPointSum = pointSum;
+//         return;
+//       }
+//     } else {
+//       for (let i = 0; i < land[depth].length; i++) {
+//         if (i === positionIndex) {
+//           continue;
+//         }
+
+//         const point = land[depth][i];
+//         bfs(point + pointSum, i, depth + 1);
+//       }
+//     }
+//   }
+
+//   for (let i = 0; i < land[0].length; i++) {
+//     const point = land[0][i];
+
+//     bfs(point, i, 1);
+//   }
+
+//   return maxPointSum;
+// }
+
+// 열의 개수가 4개라는 조건이 존재하여 중복되지 않는 인덱스에 있는 값을 더해가는 동적계획법을 활용한 풀이 방법
 function solution(land) {
   for (let i = land.length - 2; i >= 0; i--) {
     land[i][0] =
